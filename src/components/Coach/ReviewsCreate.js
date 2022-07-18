64e6d8c5710f68af682c2c0511faa7c8162890b7
SarahAbdeldaym <sarahabdeldaym@gmail.com>
2022-07-18 20:38:00 +0200

    added review component to the coach page

---
+++ src/components/Coach/ReviewsCreate.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+
+function ReviewsCreate(props) {
+  const [rate, setRate] = useState("");
+  const [comment, setComment] = useState("");
+
+  //Send Rate and Comment of Client on Coach
+  const submit = (e) => {
+    e.preventDefault();
+    let formData = {};
+    formData.rate = rate;
+    formData.comment = comment;
+    formData.client_id = 1;
+    formData.coach_id = props.id;
+
+    fetch(`http://127.0.0.1:8000/api/feedbacks`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        props.updateCoachComponent();
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <section class="rate-and-comment">
+        <div class="container">
+          <div class="card mb-5">
+            <div class="card-header">
+              <h5 class="card-title mb-0">
+                <i class="fa fa-star-half-alt"></i> Add Review and Comment
+              </h5>
+            </div>
+            <div class="card-body">
+              <form
+                name="review"
+                action="http://127.0.0.1:8000/api/feedbacks"
+                onSubmit={submit}
+                method="POST"
+              >
+                <div class="modal-body modal-rating">
+                  <div class="rating">
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="5"
+                      id="5"
+                      onChange={(e) => setRate(e.target.value)}
+                    />
+                    <label for="5">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="4"
+                      id="4"
+                      onChange={(e) => setRate(e.target.value)}
+                    />
+                    <label for="4">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="3"
+                      id="3"
+                      onChange={(e) => setRate(e.target.value)}
+                    />
+                    <label for="3">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="2"
+                      id="2"
+                      onChange={(e) => setRate(e.target.value)}
+                    />
+                    <label for="2">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="1"
+                      id="1"
+                      onChange={(e) => setRate(e.target.value)}
+                    />
+                    <label for="1">☆</label>
+                  </div>
+                  <h5>Review</h5>
+                  <textarea
+                    name="comment"
+                    class="form-control"
+                    id="comment"
+                    onChange={(e) => setComment(e.target.value)}
+                  ></textarea>
+                </div>
+                <div class="modal-footer">
+                  <button type="submit" class="btn btn-dark">
+                    Send Feedback
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    onClick={props.close}
+                    data-dismiss="modal"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default ReviewsCreate;
---
+++ src/components/Coach/ReviewsList.js
@@ -0,0 +1,80 @@
+import React, {useEffect, useState} from "react";
+import Moment from 'moment';
+
+function ReviewsList(props) { 
+    
+    const [reviews, setReviews] = useState([]);
+    
+    useEffect(() => {
+        getFeedbacks(props.id);
+     }, [props.feedback]);
+    
+    //Return All Feedback on Coach
+    function getFeedbacks(coachId) {    
+        fetch(`http://localhost:8000/api/feedbacks/${coachId}`,{
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+        })
+        .then((response) => response.json())
+        .then((res) => {
+            setReviews(res);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    var coachReviews;
+    if(reviews) {
+        coachReviews = reviews.map((review) => {
+            return ( 
+                <div class="col-lg-12">
+                    <div class="previous-rating-content p-4 my-2">
+                        <div class="row">
+                            <div class="col-sm-10">
+                                <h4 class="my-2">{review.client.name_en}</h4>
+                                <p class="my-2">{review.comment}</p>
+                                <h6 class="my-2">{Moment(review.created_at).format('YYYY-MM-DD HH:mm')}</h6>
+                            </div>
+                            <div class="col-sm-2">
+                                <div class="rating text-center">
+                                    <i class="fas fa-star fa-3x">
+                                        <span>{review.rate}</span>
+                                    </i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )});
+    } else {
+        coachReviews = () => {
+            return (
+                <p>No Rates Yet</p>
+            )};
+    }
+
+    return (
+        <React.Fragment>
+           <section class="rate-and-comment">
+                <div class="container">
+                    <div class="card mb-5">
+                        <div class="card-header">
+                            <h5 class="card-title mb-0"><i class="fa fa-star-half-alt"></i> Reviews</h5>
+                        </div>
+                        <div class="card-body">
+                            <div class="previous-rating">
+                                <div class="row">
+                                    {coachReviews} 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
+
+export default ReviewsList;
